cf79851242c38843e570f65777b2b48339957ced
TavernCreative 030322 <taverncreative030322@C-Mac.lan>
2026-03-15 08:23:09 +0000

    feat: updated favicon to new gold diamond design

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,15 +1,9 @@
 import { ImageResponse } from 'next/og';
-import fs from 'fs';
-import path from 'path';
 
-export const size = { width: 100, height: 100 };
+export const size = { width: 512, height: 512 };
 export const contentType = 'image/png';
 
 export default function Icon() {
-  const logoPath = path.resolve('./public/logo.png');
-  const buffer = fs.readFileSync(logoPath);
-  const base64 = buffer.toString('base64');
-
   return new ImageResponse(
     (
       <div
@@ -22,7 +16,12 @@ export default function Icon() {
           justifyContent: 'center',
         }}
       >
-        <img src={`data:image/png;base64,${base64}`} width="70" height="70" style={{ objectFit: 'contain' }} />
+        <svg width="400" height="400" viewBox="0 0 100 100">
+           {/* Perfect 45-degree diamond, slightly scaled down to allow padding from edge if needed - actually the image diamond hits close to the edge */}
+           <path d="M50 0 L100 50 L50 100 L0 50 Z" fill="#D6AD67" />
+           {/* Perfect mathematical checkmark in black */}
+           <path d="M47 75 L77 45 L67 35 L47 55 L32 40 L22 50 Z" fill="black" />
+        </svg>
       </div>
     ),
     { ...size }
